f460feed048340b6acd233021e92dbf336a7ee70
Paige Morton <paigeashley624@gmail.com>
2021-04-10 19:41:34 -0400

    adds images to movie table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_14_232116) do
+ActiveRecord::Schema.define(version: 2021_04_09_145227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2021_03_14_232116) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "director"
     t.boolean "english"
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
